c65934310649aa546631595cc005829248293524
sanekprofit <zaev-alex2006@yandex.ru>
2023-01-24 18:45:07 +0300

    refactor: Вынести /films в аннотацию @RequestMapping и добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmController.java
@@ -2,54 +2,53 @@ package ru.yandex.practicum.filmorate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.EmptyArrayException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RestController
+@RequestMapping(value = "/films")
 public class FilmController {
-    private Map<Integer, Film> films = new HashMap<>();
+    ValidationCheck validationCheck = new ValidationCheck();
+    private final Map<Integer, Film> films = new HashMap<>();
     private int generatorId = 0;
 
-    @GetMapping("/films")
+    @GetMapping
     public List<Film> getAllFilms() {
-        for (Film film : films.values()) {
-            return List.of(film);
+        log.info("Получен GET запрос");
+        if (films.isEmpty()) {
+            throw new EmptyArrayException("Список фильмов пуст.");
         }
-        return null;
+        return new ArrayList<>(films.values());
     }
 
-    @PostMapping(value = "/films")
+    @PostMapping
     public Film createFilm(@RequestBody Film film) {
-        if (film.getName().isEmpty()) {
-            throw new ValidationException("Название не может быть пустым.");
-        }
-        if (film.getDescription().length() > 200) {
-            throw new ValidationException("Максимальная длина описания не может быть больше 200 символов.");
-        }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года.");
-        }
-        if (film.getDuration() <= 0) {
-            throw new ValidationException("Продолжительность фильма должна быть положительной.");
-        }
+        log.info("Получен POST запрос");
+        validationCheck.validationCheckFilm(film);
         generatorId++;
         film.setId(generatorId);
         films.put(film.getId(), film);
+        log.info("Список фильмов после добавления фильма: " + getAllFilms());
         return film;
     }
 
-    @PutMapping(value = "/films")
+    @PutMapping
     public Film updateFilm(@RequestBody Film film) {
+        log.info("Получен PUT запрос");
         if (film.getId() != films.get(film.getId()).getId()) {
             throw new ValidationException("Ид не совпадают.");
         }
+        validationCheck.validationCheckFilm(film);
+        log.info("Фильм до правок: " + films.get(film.getId()));
         films.put(film.getId(), film);
+        log.info("Фильм после правок: " + films.get(film.getId()));
         return film;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/UserController.java
@@ -2,54 +2,53 @@ package ru.yandex.practicum.filmorate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.EmptyArrayException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
-    private Map<Integer, User> users = new HashMap<>();
+    ValidationCheck validationCheck = new ValidationCheck();
+    private final Map<Integer, User> users = new HashMap<>();
     private int generatorId = 0;
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers() {
-        for (User user : users.values()) {
-            return List.of(user);
+        log.info("Получен GET запрос");
+        if (users.isEmpty()) {
+            throw new EmptyArrayException("Список пользователей пуст.");
         }
-        return null;
+        return new ArrayList<>(users.values());
     }
 
-    @PostMapping(value = "/users")
+    @PostMapping
     public User createUser(@RequestBody User user) {
-        if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
-            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @.");
-        }
-        if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
-        }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем.");
-        }
-        if (user.toString().contains("name=null")) {
-            user.setName(user.getLogin());
-        }
+        log.info("Получен POST запрос");
+        validationCheck.validationCheckUser(user);
         generatorId++;
         user.setId(generatorId);
         users.put(user.getId(), user);
+        log.info("Список пользователей после добавление нового юзера: " + getUsers());
         return user;
     }
 
-    @PutMapping(value = "/users")
+    @PutMapping
     public User updateUser(@RequestBody User user) {
+        log.info("Получен PUT запрос");
         if (user.getId() != users.get(user.getId()).getId()) {
             throw new ValidationException("Ид не совпадают.");
         }
+        validationCheck.validationCheckUser(user);
+        log.info("Пользователь до изменений: " + users.get(user.getId()));
         users.put(user.getId(), user);
+        log.info("Изменённый пользователь: " + users.get(user.getId()));
         return user;
     }
 }
