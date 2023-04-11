39d837a475954390a807357b2216f23c2ce13f09
sanekprofit <zaev-alex2006@yandex.ru>
2023-04-12 02:39:38 +0300

    delete: Удалить класс EmptyArrayException из-за ненадобности

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/EmptyArrayException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class EmptyArrayException extends RuntimeException{
-    public EmptyArrayException(String s) {
-        super(s);
-    }
-}
