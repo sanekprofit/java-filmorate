05b4b8e3a3d8b5356f1dec9b60dd3ab0079be2be
sanekprofit <zaev-alex2006@yandex.ru>
2023-04-12 03:18:22 +0300

    Revert "feat: Добавить новые исключения"
    This reverts commit 66722463197262f7ee98857cb9486f6d7f230b6a.

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IdShouldntEqualsException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class IdShouldntEqualsException extends RuntimeException{
-    public IdShouldntEqualsException(String s) {
-        super(s);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String s) {
-        super(s);
-    }
-}
