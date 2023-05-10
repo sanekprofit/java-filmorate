5a2d124108cbbf3e41cfec120b82186ece49ba87
sanekprofit <zaev-alex2006@yandex.ru>
2023-05-10 20:12:27 +0300

    Подправил классы

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/GenreDbStorageImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/GenreDbStorageImpl.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.storage.GenreDbStorage;
 import ru.yandex.practicum.filmorate.model.Genre;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class GenreDbStorageImpl implements GenreDbStorage {
     private final JdbcTemplate jdbcTemplate;
@@ -26,12 +30,18 @@ public class GenreDbStorageImpl implements GenreDbStorage {
 
     @Override
     public Genre getGenreById(Integer id) {
-        String sql = "SELECT * FROM genre WHERE genre_id = ?";
-        return jdbcTemplate.query(sql, (rs) -> {
-            Genre genre = new Genre();
-            genre.setId(rs.getInt("genre_id"));
-            genre.setTitle(rs.getString("title"));
-            return genre;
-        }, id);
+        List<Integer> genreIds = jdbcTemplate.queryForList("SELECT genre_id FROM genre", Integer.class);
+        if (genreIds.contains(id)) {
+            String sql = "SELECT * FROM genre WHERE genre_id = ?";
+            return jdbcTemplate.query(sql, (rs) -> {
+                Genre genre = new Genre();
+                genre.setId(rs.getInt("genre_id"));
+                genre.setTitle(rs.getString("title"));
+                return genre;
+            }, id);
+        } else {
+            log.error("Ошибка 404, не найден жанр с ID " + id);
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/MpaDbStorageImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/MpaDbStorageImpl.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.storage.MpaDbStorage;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class MpaDbStorageImpl implements MpaDbStorage {
     private final JdbcTemplate jdbcTemplate;
@@ -26,12 +30,18 @@ public class MpaDbStorageImpl implements MpaDbStorage {
 
     @Override
     public Mpa getMpaById(Integer id) {
-        String sql = "SELECT * FROM mpa WHERE mpa_id = ?";
-        return jdbcTemplate.query(sql, (rs) -> {
-            Mpa mpa = new Mpa();
-            mpa.setId(rs.getInt("mpa_id"));
-            mpa.setTitle(rs.getString("title"));
-            return mpa;
-        }, id);
+        List<Integer> mpaIds = jdbcTemplate.queryForList("SELECT mpa_id FROM mpa", Integer.class);
+        if (mpaIds.contains(id)) {
+            String sql = "SELECT * FROM mpa WHERE mpa_id = ?";
+            return jdbcTemplate.query(sql, (rs) -> {
+                Mpa mpa = new Mpa();
+                mpa.setId(rs.getInt("mpa_id"));
+                mpa.setTitle(rs.getString("title"));
+                return mpa;
+            }, id);
+        } else {
+            log.error("Ошибка 404, не найден рейтинг MPA с ID " + id);
+            throw new IllegalArgumentException();
+        }
     }
 }
