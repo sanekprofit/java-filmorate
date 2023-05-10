a7847724dde31981717031e5b4f0715d0f588eac
sanekprofit <zaev-alex2006@yandex.ru>
2023-05-10 20:43:27 +0300

    Изменил data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO mpa
+MERGE INTO mpa
 VALUES (1, 'G'),
        (2, 'PG'),
        (3, 'PG-13'),
        (4, 'R'),
        (5, 'NC-17');
-INSERT INTO genre
+MERGE INTO genre
 VALUES (1, 'Комедия'),
        (2, 'Драма'),
        (3, 'Мультфильм'),
